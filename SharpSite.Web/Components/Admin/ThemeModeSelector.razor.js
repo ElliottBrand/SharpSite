3e36059d0b0ae4d085dc24bcb1e07484ab7f5ea2
Steve Elliott <steve_r_elliott@msn.com>
2024-12-22 17:09:24 -0500

    Admin theme checkpoint.

---
+++ SharpSite.Web/Components/Admin/ThemeModeSelector.razor.js
@@ -0,0 +1,22 @@
+﻿class ThemeModeSelectorScripts {
+    setThemeMode(isDarkMode) {
+        try {
+            const container = document.getElementById("sharpsite-admin-layout");
+            if (!container) return;
+
+            if (container.classList.contains("theme--dark"))
+                container.classList.remove("theme--dark");
+
+            if (container.classList.contains("theme--light"))
+                container.classList.remove("theme--light");
+
+            container.classList.add(isDarkMode === true ? "theme--dark" : "theme--light");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export function getThemeModeScripts() {
+    return new ThemeModeSelectorScripts();
+}
