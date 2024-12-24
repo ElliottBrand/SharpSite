7bf753c529042c32083f9e467ddd721fa0a0ebed
Steve Elliott <steve_r_elliott@msn.com>
2024-12-24 14:12:59 -0500

    A little adjustment so mobile menu doesn't push elements off page and added a rough implementation of RTL support for testing.

---
+++ SharpSite.Web/Components/Layout/Admin/AdminLayoutHeader.razor.js
@@ -0,0 +1,47 @@
+﻿class AdminLayoutScripts {
+    constructor() {
+        // NOTE: Just patching an issue with the DOM completely reloading on every page change.
+        // The observer puts the direction back in when it's removed.
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                const element = document.getElementsByTagName("html")[0];
+                if (!element) return;
+
+                if (element.getAttribute("dir"))
+                    return;
+
+                // "dir" attribute must have been removed.
+                this.setDirectionByLanguage();
+            });
+        });
+        observer.observe(document.getElementsByTagName("html")[0], { attributes: true, attributeFilter: ["dir"] });
+    }
+
+    setDirectionByLanguage() {
+        try {
+            const userLanguage = navigator.language || navigator.userLanguage;
+            const direction = this.isRtlLanguage(userLanguage) ? "rtl" : "ltr";
+
+            const element = document.getElementsByTagName("html")[0];
+            if (!element) {
+                console.warn("html element not found in DOM.");
+                return;
+            }
+
+            element.setAttribute("dir", direction);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    isRtlLanguage(language) {
+        // Common RTL languanges.
+        const rtlLanguages = ["ar", "he", "fa", "ur"];
+
+        return rtlLanguages.includes(language.split("-")[0]);
+    }
+}
+
+export function getAdminLayoutScripts() {
+    return new AdminLayoutScripts();
+}
